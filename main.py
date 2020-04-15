62ec3c6331144507ff784de32d46e24916477e29
onurbayraktar <obayraktar17@gmail.com>
2020-04-15 20:29:36 +0300

    Refactor: findMostCommonKMers & Feat: Clearing contents
    findMostCommonKMers function refactored and divided its task to some other functions.
    A function was added about clearing previous contents of the output files for simplicity.

--- main.py
+++ main.py
@@ -7,25 +7,33 @@ import re
 NUCLEOTIDES = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G', 'a': 't', 't': 'a', 'g': 'c', 'c': 'g'}
 
 
+# Class is used to reading / writing operations to / from file
 class FileManipulator:
 
     # Function to read the given input txt file #
     def readSequence(self, fileName):
         file = open(fileName, "r")
-        contents = file.readlines()             # Reading txt file line by line
+        contents = file.readlines()  # Reading txt file line by line
         sequence = ""
         for line in contents:
             output = re.sub(r'(\d)+', '', line)  # Removing digits #
             output = re.sub(r'(\s)+', '', output)  # Removing whitespaces #
             sequence += output
-        if sequence.find(":"):                  # If there's a ":" in our string; then we need to split and take the rest
+        if sequence.find(":"):  # If there's a ":" in our string; then we need to split and take the rest
             sequence = sequence.split(":")[1]
         return sequence
 
-    # Function to write the sequence to another file ( testing purposes )
+    # Function to write the sequence to another file #
     def writeSequence(self, fileName, contents):
-        file = open(fileName, "w")
+        file = open(fileName, "a")
         file.write(contents)
+        file.write("\n")
+        file.close()
+
+    # Function to clear the contents of the file #
+    def clearFileContents(self, fileName):
+        file = open(fileName, "w")
+        file.truncate(0)
         file.close()
 
     # Function to convert given sequence to string #
@@ -33,6 +41,8 @@ class FileManipulator:
         sequence = list(sequence)
         return sequence
 
+
+# Class to represent the KMer objects #
 class KMer:
     def __init__(self, string, reverse, stringFrequency, totalFrequency):
         self.string = string
@@ -50,48 +60,57 @@ def findListOfKMers(sequence, kValue):
     listOfKMers = []
     for i in range(lengthOfSequence - kValue + 1):
         listOfKMers.append(sequence[i:i + kValue])
-    #listOfKMers = [sequence[i:i + kValue] for i in range(lengthOfSequence - kValue + 1)]
     return listOfKMers
 
 
+def checkTerminatingCondition(frequenciesOfKMers):
+    if max(frequenciesOfKMers.values()) == 1:
+        return True
+    return False
+
+
 def findMostCommonKMers(sequence):
-    kValue = 5                          # Initial kValue #
-    lengthOfSequence = len(sequence)
-    listOfMostCommonKMers = []                    # Dictionary to keep KMer instances #
-    isEnd = False                       # Terminating condition #
+    kValue = 5  # Initial kValue #
+    listOfMostCommonKMers = []  # Dictionary to keep KMer instances #
+    isEnd = False  # Terminating condition #
 
     while not isEnd:
-        listOfKMers = findListOfKMers(sequence, kValue)     # Extracting the list of kMers for a given kValue.
-        frequenciesOfKMers = Counter(listOfKMers)           # Finding the frequencies of each kMers.
-        mostCommonKMers = frequenciesOfKMers.most_common(5) # Fetch the 5 one that has the most frequency.
+        listOfKMers = findListOfKMers(sequence, kValue)  # Extracting the list of kMers for a given kValue.
+        frequenciesOfKMers = Counter(listOfKMers)  # Finding the frequencies of each kMers.
+        mostCommonKMers = frequenciesOfKMers.most_common(5)  # Fetch the 5 one that has the most frequency.
 
-        for kMer in mostCommonKMers:                        # For each kMer; we'll find reverse comp and freqs.
+        if checkTerminatingCondition(frequenciesOfKMers):  # Checking if we should terminate or go on
+            isEnd = True
+        else:
+            kValue += 1                             # If didn't terminate, then we need to inc kValue
+
+        for kMer in mostCommonKMers:  # For each kMer; we'll find reverse comp and freqs.
+            if kMer[1] == 1:
+                break
             reverseCompliment = findReverseComplimentOfKMer(kMer[0])
             reverseFreq = sequence.count(reverseCompliment)
             kMerObject = KMer(kMer[0], reverseCompliment, kMer[1], reverseFreq)
             listOfMostCommonKMers.append(kMerObject)
 
-        if max(frequenciesOfKMers.values()) == 1:           # If the maximum frequency equals 1; then we don't need to search more
-            isEnd = True
-        else:
-            kValue += 1
-
     return listOfMostCommonKMers
 
 
 def main():
+    # Variables to be used #
     listOfFileNames = ["WUHAN.txt", "NEPAL.txt"]
     listOfFileNamesForFormatted = ["wuhanOutput.txt", "nepalOutput.txt"]
     fileManipulator = FileManipulator()
 
+    # We need to process each file; so we have a loop in here #
     for file in range(2):
+        fileManipulator.clearFileContents(listOfFileNamesForFormatted[file])    # Clearing the previous contents #
         currentFile = listOfFileNames[file]
         currentSequence = fileManipulator.readSequence(currentFile)
-        fileManipulator.writeSequence(listOfFileNamesForFormatted[file], currentSequence)
         kMersReturned = findMostCommonKMers(currentSequence)
         for kMer in kMersReturned:
-            print(kMer.string + " : " + kMer.reverse + " : " + str(kMer.stringFrequency) + " : " + str(kMer.totalFrequency))
-
+            aboutToWrite = kMer.string + " : " + kMer.reverse + " : " + str(kMer.stringFrequency) + " : " + str(
+                kMer.totalFrequency)
+            fileManipulator.writeSequence(listOfFileNamesForFormatted[file], aboutToWrite)
         print()
         print()
         print()
@@ -99,28 +118,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-'''
-
-
-def main():
-
-    for i in range(2):
-
-        print()
-        print()
-        print()
-        print("THE MOST FREQUENT KMERS AND FREQUENCIES")
-        for kMer in dictOfKMerFrequency:
-            pureFrequency = dictOfKMerFrequency[kMer]
-            reverseComp = findReverseCompOfKMer(kMer, NUCLEOTIDES)
-            reverseFreq = currentSequence.count(reverseComp)
-            totalFreq = pureFrequency + reverseFreq
-
-            print(kMer + " : " + str(pureFrequency))
-            print(kMer + " : " + str(totalFreq))
-            print()
-
-
-if __name__ == '__main__':
-    main()
-'''
